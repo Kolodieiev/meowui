22c7359b8ca622adba16c8611a9d82fa78cf51d9
Kolodieiev <valerij.kolodeev@gmail.com>
2025-02-22 01:33:19 +0200

    Додано документацію

--- src/meow/lib/i2s_out/I2SWriter.h
+++ src/meow/lib/i2s_out/I2SWriter.h
@@ -10,8 +10,30 @@ namespace meow
     public:
         ~I2SWriter() { deinit(); }
 
+        /**
+         * @brief Ініціалізує шину I2S в режимі виводу.
+         *
+         * @param port Порт, на якому буде ініціалізована шина.
+         * @param buff_size Розмір буферу.
+         * @param sample_rate Кількість вибірок за секунду.
+         * @return true - Якщо ініціалізацію виконано успішно.
+         * @return false - Інакше.
+         */
         bool init(i2s_port_t port, uint16_t buff_size, uint16_t sample_rate = 16000);
+
+        /**
+         * @brief Деініціалізує ініціалізовану раніше шину I2S.
+         *
+         */
         void deinit();
+
+        /**
+         * @brief Відправляє дані по раніше налаштованій шині I2S.
+         *
+         * @param buffer Буфер даних.
+         * @param one_chann_buff Прапор, який вказує на кількість каналів, що містяться у буфері. Якщо true - буфер одноканальний. Інакше двоканальний.
+         * @return size_t - Кількість записаних даних.
+         */
         size_t write(const void *buffer, bool one_chann_buff = true);
 
     private:
