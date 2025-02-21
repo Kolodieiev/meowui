63ed80be5eccd2f49842edcba7be60c091e42eaf
Kolodieiev <valerij.kolodeev@gmail.com>
2025-02-22 01:01:22 +0200

    Update i2c_setup.h

--- include/meowui_setup/i2c_setup.h
+++ include/meowui_setup/i2c_setup.h
@@ -1,1 +1,7 @@
-//TODO Додати менеджер, який буде керувати шиною
+#pragma once
+#include <stdint.h>
+
+#define PIN_I2C_SDA (uint8_t)12
+#define PIN_I2C_SCL (uint8_t)13
+
+#define I2C_AWAIT_TIME_MS (unsigned long)100 // Час очікування відповіді від i2c пристрою
