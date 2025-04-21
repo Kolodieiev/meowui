e524f0e69a54b0d7f15b8258594ff864bc04fff4
Пройдисвіт <valerij.kolodeev@gmail.com>
2025-04-22 00:53:26 +0300

    Update lua.h

--- src/meow/plugin/lua/lua.h
+++ src/meow/plugin/lua/lua.h
@@ -1,6 +1,12 @@
+#pragma once
+
 extern "C"
 {
 #include "./lua_src/lua.h"
 #include "./lua_src/lualib.h"
 #include "./lua_src/lauxlib.h"
 }
+
+#include "./lua_err_msg.h"
+
+typedef int (*LuaRegisterFunc)(lua_State *L);
