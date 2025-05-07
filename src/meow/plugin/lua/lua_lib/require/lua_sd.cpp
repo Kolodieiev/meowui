2784c7327f90bfbfbaf1e3702b46f34852c2b498
Пройдисвіт <valerij.kolodeev@gmail.com>
2025-05-08 00:41:26 +0300

    Update lua_sd.cpp

--- src/meow/plugin/lua/lua_lib/require/lua_sd.cpp
+++ src/meow/plugin/lua/lua_lib/require/lua_sd.cpp
@@ -6,7 +6,7 @@
 const char STR_FILE_TYPE[] = "FILE*";
 //
 const char STR_MEM_ALLOC_ERR[] = "Помилка створення буфера";
-const char STR_FILE_READ_ERR[] = "Помилка читання файлу";
+const char STR_FILE_READ_ERR[] = "Помилка читання файла";
 //
 const char STR_SD_IS_MOUNTED[] = "isMounted";
 const char STR_SD_FILE_EXIST[] = "fileExist";
