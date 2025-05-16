7d2944a8135a3e9599ee4baa608a464ef21b2af1
Пройдисвіт <valerij.kolodeev@gmail.com>
2025-05-16 22:17:04 +0300

    Update lua_err_msg.h

--- src/meow/plugin/lua/res/lua_err_msg.h
+++ src/meow/plugin/lua/res/lua_err_msg.h
@@ -1,5 +1,3 @@
 #pragma once
 
-const char STR_INCORRECT_ARGS_NUMBER[] = "Некоректна кількість аргументів. Очікується %d, отримано %d";
 const char STR_EMPTY_MODULE_NAME[] = "Ім'я модуля не може бути null";
-const char STR_INCORRECT_TYPE_CONVERSION[] = "Некоректне перетворення типу: %s до %s";
