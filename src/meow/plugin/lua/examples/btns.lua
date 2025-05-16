ba2a9767eebbd5c11252856b256371570ccd381f
Пройдисвіт <valerij.kolodeev@gmail.com>
2025-05-16 22:10:00 +0300

    Переписано/додано приклади скриптів

--- src/meow/plugin/lua/examples/btns.lua
+++ src/meow/plugin/lua/examples/btns.lua
@@ -1,26 +1,25 @@
--- Номера кнопок, як вказано в прошивці Meowui
+-- Скрипт демонструє обробку стану кнопок.
+
+-- Номера пінів кнопок, як вказано в прошивці Meowui.
 BTN_UP = 38
-BTN_DOWN = 41
 BTN_LEFT = 39
 BTN_RIGHT = 40
 BTN_EXIT = 0
 
 function update()
-	if input.is_pressed(BTN_EXIT) then
-        input.lock(BTN_EXIT, 1000) -- Заблокувати спрацьовування на n мс
+	if input.is_pressed(BTN_EXIT) then 
+        input.lock(BTN_EXIT, 1000) -- Заблокувати спрацьовування кнопки на n мс.
         print("EXIT");
 		context.exit()
-    elseif input.is_holded(BTN_UP) then
+    elseif input.is_pressed(BTN_UP) then -- Обробка стану is_pressed повинна завжди виконуватися ДО is_holded на одній кнопці.
         input.lock(BTN_UP, 100)
-        print("UP");
-    elseif input.is_holded(BTN_DOWN) then
-        input.lock(BTN_DOWN, 100)
-        print("DOWN");
-	elseif input.is_holded(BTN_LEFT) then
-        input.lock(BTN_LEFT, 100)
-        print("LEFT");
-	elseif input.is_holded(BTN_RIGHT) then
+        print("Кнопка \"вгору\" затиснута тривалий час");
+    elseif input.is_released(BTN_UP) then
+        input.lock(BTN_UP, 100)
+        print("Відпущено кнопку \"вгору\"");
+    elseif input.is_holded(BTN_RIGHT) then
         input.lock(BTN_RIGHT, 100)
-        print("RIGHT");
+        print("Утримується кнопка \"вправо\"");
     end
+	
 end
