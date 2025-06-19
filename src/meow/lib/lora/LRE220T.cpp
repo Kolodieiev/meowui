5b31b640438f9ba17a131a422b999268dd96d04b
Пройдисвіт <valerij.kolodeev@gmail.com>
2025-06-19 23:24:15 +0300

    Додано абстракцію

--- src/meow/lib/lora/LRE220T.cpp
+++ src/meow/lib/lora/LRE220T.cpp
@@ -1,225 +1,31 @@
-#pragma GCC optimize("O3")
 #include "LRE220T.h"
 #include <HardwareSerial.h>
-#include "modules_setup/lora/LRE220.h"
+
+#define LRE220_MIN_DATA_RATE ILoRa::AIR_DATA_RATE_2_4K
+#define LRE220_MAX_DATA_RATE ILoRa::AIR_DATA_RATE_62_5K
+#define LRE220_MAX_CHAN 80
 
 namespace meow
 {
-    bool LRE220T::begin()
+    LRE220T::LRE220T() : ILoRa(LRE220_MIN_DATA_RATE, LRE220_MAX_DATA_RATE, LRE220_MAX_CHAN)
     {
-        log_i("Старт ініціалізації");
-        pinMode(PIN_LRE220_M0, OUTPUT);
-        pinMode(PIN_LRE220_M1, OUTPUT);
-        pinMode(PIN_LRE220_AUX, INPUT);
-
-        setMode(MODE_NORMAL);
-        setMode(MODE_CONFIG);
-
-        if (readRegisters())
-        {
-            setMode(MODE_NORMAL);
-
-            log_i("LoRa ініціалізовано");
-            _is_inited = true;
-            return true;
-        }
-
-        log_e("Помилка ініціалізації");
-        _is_inited = false;
-        return false;
     }
 
-    void LRE220T::end()
-    {
-        setMode(MODE_CONFIG);
-        Serial1.end();
-        _is_inited = false;
-    }
-
-    bool LRE220T::writeSettings()
-    {
-        if (!_is_inited)
-            return false;
-
-        setMode(MODE_NORMAL);
-        setMode(MODE_CONFIG);
-
-        setup();
-
-        if (!writeRegisters())
-            return false;
-
-        setMode(MODE_NORMAL);
-
-        flushBuffer();
-        log_i("Налаштування збережено");
-        return true;
-    }
-
-    void LRE220T::setTransmitPower(TransmitPower power)
-    {
-        if (power > POWER_LOW)
-            _transmit_power = POWER_LOW;
-        else
-            _transmit_power = power;
-    }
-
-    void LRE220T::setAirDataRate(AirDataRate rate)
-    {
-        if (rate < AIR_DATA_RATE_2_4K)
-            _air_data_rate = AIR_DATA_RATE_2_4K;
-        else if (rate > AIR_DATA_RATE_62_5K)
-            _air_data_rate = AIR_DATA_RATE_62_5K;
-        else
-            _air_data_rate = rate;
-    }
-
-    void LRE220T::setPacketLen(PacketLen len)
-    {
-        if (len > PACK_LEN_32B)
-            len = PACK_LEN_32B;
-
-        _packet_len = len;
-
-        switch (len)
-        {
-        case PACK_LEN_200B:
-            _real_packet_len = 200;
-            break;
-
-        case PACK_LEN_128B:
-            _real_packet_len = 128;
-            break;
-
-        case PACK_LEN_64B:
-            _real_packet_len = 64;
-            break;
-
-        case PACK_LEN_32B:
-            _real_packet_len = 32;
-            break;
-        }
-    }
-
-    void LRE220T::warmUp()
-    {
-        if (!_is_inited)
-            return;
-
-        waitBusy();
-        uint8_t buff = 0;
-        Serial1.write(&buff, 1);
-    }
-
-    void LRE220T::writePacket(const uint8_t *buff)
-    {
-        if (!_is_inited)
-            return;
-
-        waitBusy();
-        Serial1.write(buff, _real_packet_len);
-    }
-
-    bool LRE220T::readPacket(uint8_t *out_buff)
-    {
-        uint8_t data_counter = 0;
-
-        while (Serial1.available())
-            out_buff[data_counter++] = Serial1.read();
-
-        return data_counter == _real_packet_len;
-    }
-
-    bool LRE220T::packetAvailable()
-    {
-        if (Serial1.available() > 0)
-        {
-            if (Serial1.available() < _real_packet_len)
-            {
-                delay(5);
-                return Serial1.available() == _real_packet_len;
-            }
-
-            return Serial1.available() == _real_packet_len;
-        }
-
-        return false;
-    }
-
-    void LRE220T::flushBuffer()
-    {
-        while (Serial1.available())
-            Serial1.read();
-    }
-
-    void LRE220T::setMode(LRE220T_Mode mode)
-    {
-        waitBusy();
-
-        if (mode == MODE_NORMAL)
-        {
-            Serial1.end();
-            Serial1.begin(NORMAL_BAUD_RATE, SERIAL_8N1, PIN_LRE220_TX, PIN_LRE220_RX);
-
-            digitalWrite(PIN_LRE220_M0, LOW);
-            digitalWrite(PIN_LRE220_M1, HIGH);
-            delay(20);
-            digitalWrite(PIN_LRE220_M0, LOW);
-            digitalWrite(PIN_LRE220_M1, LOW);
-            delay(80);
-
-            _mode = MODE_NORMAL;
-        }
-        else if (mode == MODE_CONFIG)
-        {
-            Serial1.end();
-            Serial1.begin(CONFIG_BAUD_RATE, SERIAL_8N1, PIN_LRE220_TX, PIN_LRE220_RX);
-
-            digitalWrite(PIN_LRE220_M0, HIGH);
-            digitalWrite(PIN_LRE220_M1, HIGH);
-            delay(100);
-
-            _mode = MODE_CONFIG;
-        }
-        else
-        {
-            log_e("Unknown mode");
-            esp_restart();
-        }
-    }
-
-    void LRE220T::setChannel(uint8_t channel)
-    {
-        channel > 80 ? _channel = 80 : _channel = channel;
-    }
-
-    bool LRE220T::isBusy()
-    {
-        return digitalRead(PIN_LRE220_AUX) != HIGH;
-    }
-
-    void LRE220T::waitBusy()
-    {
-        while (isBusy())
-        {
-        }
-    }
-
-    void LRE220T::setup()
+    void LRE220T::configShadowReg()
     {
         _reg_0->addr_high = 0;
         _reg_1->addr_low = 0;
         //
         _reg_2->serial_rate = 7;
         _reg_2->pairity_bit = 0;
-        _reg_2->air_data_rate = _air_data_rate;
+        _reg_2->air_data_rate = getAirDataRate();
         //
-        _reg_3->packet_len = _packet_len;
+        _reg_3->packet_len = 2;
         _reg_3->rssi_amb_en = 0;
         _reg_3->RESERVED = 0;
-        _reg_3->transmit_power = _transmit_power;
+        _reg_3->transmit_power = getTransmitPower();
         //
-        _reg_4->channel = _channel;
+        _reg_4->channel = getChannel();
         //
         _reg_5->rssi_byte_en = 0;
         _reg_5->transmit_meth = 0,
@@ -232,64 +38,67 @@ namespace meow
         _reg_7->crypt_key_l = 0;
     }
 
-    bool LRE220T::readRegisters()
+    bool LRE220T::writeRegisters(bool temporary)
     {
-        log_i("Read registers");
-        waitBusy();
+        waitWhileBusy();
 
-        uint8_t cmd[] = {193, 0, sizeof(_shadow_registers)};
-        Serial1.write(cmd, sizeof(cmd));
+        uint8_t cmd[3];
 
-        if (!waitResponce(sizeof(cmd) + sizeof(_shadow_registers)))
-            return false;
+        if (temporary)
+            cmd[0] = 0xC2;
+        else
+            cmd[0] = 0xC0;
 
-        Serial1.readBytes(cmd, sizeof(cmd));
-        Serial1.readBytes(_shadow_registers, sizeof(_shadow_registers));
-
-        return true;
-    }
-
-    bool LRE220T::writeRegisters()
-    {
-        log_i("Write registers");
-
-        waitBusy();
-
-        uint8_t cmd[] = {192, 0, sizeof(_shadow_registers)};
-        uint8_t temp_cmd[sizeof(cmd) + sizeof(_shadow_registers)];
-        memcpy(temp_cmd, cmd, sizeof(cmd));
-        memcpy(temp_cmd + sizeof(cmd), _shadow_registers, sizeof(_shadow_registers));
+        cmd[1] = 0;
+        cmd[2] = sizeof(_shadow_registers);
 
         // log_i("");
-        // for (int i = 3; i < sizeof(temp_cmd); i++)
+        // log_i("Write registers");
+        // for (int i = 0; i < sizeof(_shadow_registers); ++i)
         // {
-        //     log_i("%02X ", temp_cmd[i]);
+        //     log_i("%02X ", _shadow_registers[i]);
         // }
         // log_i("");
 
-        Serial1.write(temp_cmd, sizeof(temp_cmd));
+        Serial1.write(cmd, sizeof(cmd));
+        Serial1.write(_shadow_registers, sizeof(_shadow_registers));
 
-        if (!waitResponce(sizeof(temp_cmd)))
+        if (!waitResponce(sizeof(sizeof(cmd) + sizeof(_shadow_registers))))
+        {
+            log_e("Помилка запису регістрів LoRa");
             return false;
+        }
 
         flushBuffer();
 
         return true;
     }
 
-    bool LRE220T::waitResponce(size_t resp_size)
+    bool LRE220T::readRegisters()
     {
-        unsigned long start_time = millis();
+        waitWhileBusy();
 
-        while (Serial1.available() != resp_size)
+        uint8_t cmd[]{0xC1, 0, sizeof(_shadow_registers)};
+        Serial1.write(cmd, sizeof(cmd));
+
+        if (!waitResponce(sizeof(cmd) + sizeof(_shadow_registers)))
         {
-            if (millis() - start_time > 250)
-            {
-                log_e("LoRa не відповідає або невірна команда");
-                return false;
-            }
+            log_e("Помилка читання регістрів LoRa");
+            return false;
         }
+
+        Serial1.readBytes(cmd, sizeof(cmd));
+        Serial1.readBytes(_shadow_registers, sizeof(_shadow_registers));
+
+        // log_i("");
+        // log_i("Read registers");
+        // for (int i = 0; i < sizeof(_shadow_registers); ++i)
+        // {
+        //     log_i("%02X ", _shadow_registers[i]);
+        // }
+        // log_i("");
 
         return true;
     }
+
 } // namespace meowui
--- src/meow/lib/lora/LRE220T.h
+++ src/meow/lib/lora/LRE220T.h
@@ -1,206 +1,21 @@
 #pragma once
-#pragma GCC optimize("O3")
 #include <Arduino.h>
+#include "./ILoRa.h"
 
 namespace meow
 {
-    class LRE220T
+    class LRE220T : public ILoRa
     {
     public:
-        // ----------------------------------------------------------------
+        LRE220T();
+        virtual ~LRE220T() override {}
 
-        enum TransmitPower : uint8_t
-        {
-            POWER_MAX = 0,
-            POWER_HIGH,
-            POWER_MID,
-            POWER_LOW
-        };
-
-        enum LRE220T_Mode : uint8_t
-        {
-            MODE_CONFIG,
-            MODE_NORMAL
-        };
-
-        enum AirDataRate : uint8_t
-        {
-            AIR_DATA_RATE_2_4K = 2,
-            AIR_DATA_RATE_4_8K = 3,
-            AIR_DATA_RATE_9_6K = 4,
-            AIR_DATA_RATE_19_2K = 5,
-            AIR_DATA_RATE_38_4K = 6,
-            AIR_DATA_RATE_62_5K = 7
-        };
-
-        enum PacketLen : uint8_t
-        {
-            PACK_LEN_200B = 0,
-            PACK_LEN_128B = 1,
-            PACK_LEN_64B = 2,
-            PACK_LEN_32B = 3
-        };
-
-        // ----------------------------------------------------------------
-
-        /**
-         * @brief Ініціалізує шину UART для Serial1 та модуль LoRa, який повинен бути підключеним до цієї шини.
-         *
-         * @return true - В разі успіху ініціалізації.
-         * @return false - Інакше.
-         */
-        bool begin();
-
-        /**
-         * @brief Деініціалізує шину UART для Serial1 та переводить модуль LoRa в режим сну.
-         *
-         */
-        void end();
-
-        /**
-         * @brief Записує усі налаштування модуля в його тимчасові регістри.
-         *
-         * @return true - В разі успіху запису.
-         * @return false - Інакше.
-         */
-        bool writeSettings();
-
-        // ----------------------------------------------------------------
-
-        /**
-         * @brief Встановлює режим для модуля.
-         *
-         * @param mode Може мати значення MODE_CONFIG або MODE_NORMAL.
-         */
-        void setMode(LRE220T_Mode mode);
-
-        /**
-         * @brief Повертає поточний режим роботи модуля.
-         *
-         * @return LRE220T_Mode
-         */
-        LRE220T_Mode getMode() const { return _mode; }
-
-        // ----------------------------------------------------------------
-
-        /**
-         * @brief Встановлює номер каналу, на якому буде працювати модуль.
-         * Цей метод змінює тільки локальне налаштування.
-         * Для запису налаштувань в модуль, необхідно викликати writeSettings().
-         *
-         * @param channel Може мати значення від 0 - 80.
-         */
-        void setChannel(uint8_t channel);
-
-        /**
-         * @brief Повертає локальне значення налаштування номеру каналу.
-         *
-         * @return uint8_t
-         */
-        uint8_t getChannel() const { return _channel; }
-
-        // ----------------------------------------------------------------
-
-        /**
-         * @brief Встановлює потужність передавача.
-         * Цей метод змінює тільки локальне налаштування.
-         * Для запису налаштувань в модуль, необхідно викликати writeSettings().
-         *
-         * @param power
-         */
-        void setTransmitPower(TransmitPower power);
-
-        /**
-         * @brief Повертає локальне значення налаштування потужності передавача.
-         *
-         * @return TransmitPower
-         */
-        TransmitPower getTransmitPower() const { return _transmit_power; }
-
-        // ----------------------------------------------------------------
-
-        /**
-         * @brief Встановлює швидкість передачі даних модулем.
-         * Цей метод змінює тільки локальне налаштування.
-         * Для запису налаштувань в модуль, необхідно викликати writeSettings().
-         *
-         * @param rate
-         */
-        void setAirDataRate(AirDataRate rate);
-
-        /**
-         * @brief Повертає локальне значення налаштування швидкості передачі даних.
-         *
-         * @return AirDataRate
-         */
-        AirDataRate getAirDataRate() const { return _air_data_rate; }
-
-        // ----------------------------------------------------------------
-
-        /**
-         * @brief Встановлює розмір пакета даних у байтах.
-         * Цей метод змінює тільки локальне налаштування.
-         * Для запису налаштувань в модуль, необхідно викликати writeSettings().
-         *
-         * @param len
-         */
-        void setPacketLen(PacketLen len);
-
-        /**
-         * @brief Повертає локальне значення налаштування розміру пакета даних із перечислення.
-         *
-         * @return PacketLen
-         */
-        PacketLen getPacketLen() const { return _packet_len; }
-
-        /**
-         * @brief Повертає локальне значення налаштування розміру пакета даних у байтах.
-         *
-         * @return uint8_t
-         */
-        uint8_t getRealPacketLen() const { return _real_packet_len; }
-
-        // ----------------------------------------------------------------
-
-        /**
-         * @brief Надсилає пакет, що містить один байт даних.
-         *
-         */
-        void warmUp();
-
-        /**
-         * @brief Надсилає пакет, в який буде скопійовано байти із буферу, відповідно до налаштувань модуля.
-         *
-         * @param buff Буфер, що містить дані для пакета.
-         */
-        void writePacket(const uint8_t *buff);
-
-        /**
-         * @brief Читає останній пакет із модуля LoRa в буфер.
-         *
-         * @param out_buff Вихідний буфер, в який буде записано пакет.
-         * @return true - Якщо кількість прочитаних байтів співпадає з розміром пакета, що вказаний в налаштуваннях модуля.
-         * @return false - Інакше.
-         */
-        bool readPacket(uint8_t *out_buff);
-
-        /**
-         * @brief Перевіряє наявність доступного пакету у буфері даних LoRa.
-         *
-         * @return true - Якщо у буфері є мінімум один пакет даних.
-         * @return false - Інакше.
-         */
-        bool packetAvailable();
-
-        /**
-         * @brief Очищує усі вхідні дані із буфера LoRa.
-         *
-         */
-        void flushBuffer();
+    protected:
+        virtual void configShadowReg() override;
+        virtual bool writeRegisters(bool temporary) override;
+        virtual bool readRegisters() override;
 
     private:
-        // ----------------------------------------------------------------
-
         struct REG_0
         {
             uint8_t addr_high{0};
@@ -251,36 +66,16 @@ namespace meow
             uint8_t crypt_key_l{0};
         };
 
-        bool isBusy();
-
-        void setup();
-        bool readRegisters();
-        bool writeRegisters();
-        bool waitResponce(size_t resp_size);
-        void waitBusy();
-
     private:
-        const uint32_t CONFIG_BAUD_RATE = 9600;
-        const uint32_t NORMAL_BAUD_RATE = 115200;
-
-        REG_0 *_reg_0 = (REG_0 *)&_shadow_registers[0];
-        REG_1 *_reg_1 = (REG_1 *)&_shadow_registers[1];
-        REG_2 *_reg_2 = (REG_2 *)&_shadow_registers[2];
-        REG_3 *_reg_3 = (REG_3 *)&_shadow_registers[3];
-        REG_4 *_reg_4 = (REG_4 *)&_shadow_registers[4];
-        REG_5 *_reg_5 = (REG_5 *)&_shadow_registers[5];
-        REG_6 *_reg_6 = (REG_6 *)&_shadow_registers[6];
-        REG_7 *_reg_7 = (REG_7 *)&_shadow_registers[7];
-
-        TransmitPower _transmit_power{POWER_MAX};
-        AirDataRate _air_data_rate = AIR_DATA_RATE_2_4K;
-        PacketLen _packet_len = PACK_LEN_200B;
-        LRE220T_Mode _mode{MODE_CONFIG};
-
-        uint8_t _channel{23};
-        uint8_t _real_packet_len = 200;
         uint8_t _shadow_registers[8]{};
 
-        bool _is_inited{false};
+        REG_0 *_reg_0 = reinterpret_cast<REG_0 *>(&_shadow_registers[0]);
+        REG_1 *_reg_1 = reinterpret_cast<REG_1 *>(&_shadow_registers[1]);
+        REG_2 *_reg_2 = reinterpret_cast<REG_2 *>(&_shadow_registers[2]);
+        REG_3 *_reg_3 = reinterpret_cast<REG_3 *>(&_shadow_registers[3]);
+        REG_4 *_reg_4 = reinterpret_cast<REG_4 *>(&_shadow_registers[4]);
+        REG_5 *_reg_5 = reinterpret_cast<REG_5 *>(&_shadow_registers[5]);
+        REG_6 *_reg_6 = reinterpret_cast<REG_6 *>(&_shadow_registers[6]);
+        REG_7 *_reg_7 = reinterpret_cast<REG_7 *>(&_shadow_registers[7]);
     };
 } // namespace meowui
